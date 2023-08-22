1a493a57b161d82dc89ba9e54217b2f468109550
aplusiv <50slash50@gmail.com>
2023-08-22 23:40:48 +0300

    define card system is changed

--- src/js/defineCardSystem.js
+++ src/js/defineCardSystem.js
@@ -3,6 +3,20 @@
 export default function defineCardSystem(value, handler) {
   if (handler(value)) {
     const str = String(value).replace(/\D/g, '');
+    if (/^4[0-9]{12}(?:[0-9]{3})?$/.test(str)) return 'Visa';
+    if (/^2200[0-9]{12}$/.test(str)) return 'Mir';
+    if (/^[52][1-5][0-9]{14}$/.test(str)) return 'Mastercard'; // могут начинаться с 2
+    if (/^3[47][0-9]{13}$/.test(str)) return 'American Express';
+    if (/^6(?:011|5[0-9]{2})[0-9]{12}[0-9]{3}?$/.test(str)) return 'Discover'; // 16 или 19
+    if (/^35[0-9]{14}[0-9]?$/.test(str)) return 'JCB'; // 15-16
+    if (/^3[06][0-9]{12}$/.test(str)) return 'Diners Club'; // 14
+  }
+  return false;
+}
+
+/* export default function defineCardSystem(value, handler) {
+  if (handler(value)) {
+    const str = String(value).replace(/\D/g, '');
     const firstNumber = Number(str[0]);
     const twoNumbers = Number(str.slice(0, 2));
     const fourNumbers = Number(str.slice(0, 4));
@@ -10,11 +24,6 @@ export default function defineCardSystem(value, handler) {
     if (firstNumber === 2) {
       if (fourNumbers === 2200) return 'Mir';
       return 'Mastercard';
-      /* if (fourNumbers === 2200) {
-        return 'Mir';
-      } else {
-        return 'Mastercard';
-      } */
     }
 
     if (firstNumber === 3) {
@@ -32,4 +41,4 @@ export default function defineCardSystem(value, handler) {
     return false;
   }
   return false;
-}
+} */
